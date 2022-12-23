baac91f347e8ed85d605afd3dcf7280643fad712
Mladen <mladen.ivastinovic@cobeisfresh.com>
2022-12-23 12:42:51 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CobeDependencyInjection
+
+A description of this package.
---
+++ Sources/CobeDependencyInjection/CobeDependencyInjection.swift
@@ -0,0 +1,6 @@
+public struct CobeDependencyInjection {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
